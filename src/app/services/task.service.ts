41159f5904a58c3a4a67df94273b97fbc4de1fb2
patrykosak <pionchaoo@gmail.com>
2022-03-26 19:18:39 +0100

    fetching and deleting tasks from server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
-import {TASKS} from "../mock-tasks"
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable} from 'rxjs'
 import { Task } from 'src/app/Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
   }
 }
